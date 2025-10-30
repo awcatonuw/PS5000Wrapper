2e3547e667c19d96dc6d58db4d042ec360f10c41
malulim <malulim3@gmail.com>
2025-10-30 17:03:54 -0400

    run once and it seems working.

--- Wrapper/obj/Debug/net9.0/Wrapper.AssemblyInfo.cs
+++ Wrapper/obj/Debug/net9.0/Wrapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wrapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa06fafcac03fd4cf6e2a4bf38dd393d521477cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wrapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wrapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
